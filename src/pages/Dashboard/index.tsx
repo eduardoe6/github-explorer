7c4342d571f0f0c5bc7376da5174a49b924320fe
eduardoe6 <edueduardo0699@gmail.com>
2021-03-11 13:48:13 -0300

    Adicionando funcionalidade para salvar os repositórios pesquisados no localstorage.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 import api from '../../services/api';
@@ -16,7 +16,19 @@ interface Repository {
 const Dashboard: React.FC = () => {
    const [newRepo, setNewRepo] = useState('');
    const [inputError, setInputError] = useState('');
-   const [repositories, setRepositories] = useState<Repository[]>([]);
+   const [repositories, setRepositories] = useState<Repository[]>(() => {
+      const storagedRepositories = localStorage.getItem('@github-explorer:repositories');
+
+      if (storagedRepositories) {
+         return JSON.parse(storagedRepositories);
+      } else {
+         return [];
+      }
+   });
+
+   useEffect(() => {
+      localStorage.setItem('@github-explorer:repositories', JSON.stringify(repositories));
+   }, [repositories]);
 
    async function handleAddRepository(event: FormEvent): Promise<void> {
       event.preventDefault();
@@ -50,7 +62,7 @@ const Dashboard: React.FC = () => {
          </Form>
 
          {inputError && <Error>{inputError}</Error>}
-         
+
          <Repositories>
             {repositories.map(repository => (
                <a key={repository.full_name} href="teste">
